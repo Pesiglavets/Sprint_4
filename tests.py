3dd58ba0614d8bbca165c6f6578237837fb33bda
Pesiglavets <russdebut8@gmail.com>
2025-08-26 14:57:23 +0300

    added 2nd test

--- tests.py
+++ tests.py
@@ -9,3 +9,10 @@ class TestBooksCollector:
         collector.add_new_book('Книга 2')
 
         assert len(collector.get_books_genre()) == 2
+        
+    # Добавление дубликата книги
+    def test_add_new_book_duplicate_name_not_added(self, collector):
+        collector.add_new_book('Книга 1')
+        collector.add_new_book('Книга 1')
+
+        assert len(collector.get_books_genre()) == 1      
